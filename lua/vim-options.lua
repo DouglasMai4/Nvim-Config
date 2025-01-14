fd0b927ff8925b1f0dcde9c6da33ccb4cf22cedf
DouglasMai4 <douglasmai4@protonmail.com>
2025-01-14 18:06:40 -0300

    feat: Config diagnostics dialog

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,12 +8,26 @@ vim.g.background = "dark"
 vim.opt.swapfile = true
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
+-- Number line
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+-- Diagnostic options
+vim.keymap.set("n", "<leader>c", vim.diagnostic.open_float, { noremap = true, silent = true })
+vim.api.nvim_create_autocmd("CursorHold", {
+	callback = function()
+		vim.diagnostic.open_float(nil, {
+			focusable = false,
+			border = "rounded",
+			source = "always",
+			prefix = "",
+		})
+	end,
+})
