79cbe107a3f3f9c519f4ad11fa09d5550bf688b7
DouglasMai4 <douglasmai4@protonmail.com>
2025-01-18 14:46:33 -0300

    feat: Update config for neo tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,5 +9,19 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left toggle<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      enable_diagnostics = true,
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          use_libuv_file_watcher = true
+        }
+      }
+    })
 	end,
 }
